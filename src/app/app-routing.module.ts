0a6d6417d57ee73f2b764a4ee534aba1e17f9551
Luke Beach <lb580@kent.ac.uk>
2020-02-25 18:59:52 +0100

    Deployment changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
-const routes: Routes = [{ path: '', component: PokemonListComponent }];
+const routes: Routes = [{ path: 'home', component: PokemonListComponent }, {path: '**', redirectTo: 'home'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
